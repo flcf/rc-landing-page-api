c5441fe858d62e76dd2f430daa3d6d9d52bc3b7a
flcf <tcbnar@gmail.com>
2025-03-13 21:34:45 -0700

    added event calls

--- api/calendar/updateCalendarEvents.ts
+++ api/calendar/updateCalendarEvents.ts
@@ -2,18 +2,33 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 import fetchGoogleCalendarEvents from '../../services/calendarService';
 
 export default async function updateCalendarEvents(req: VercelRequest, res: VercelResponse) {
-  const calendarId = process.env.GOOGLE_CALENDAR_ID;
+  const calendarIdVan = process.env.VANCOUVER_CALENDAR_ID;
+  const calendarIdCal = process.env.CALGARY_CALENDAR_ID;
 
-    if (!calendarId) {
-        return res.status(400).json({ error: 'Calendar ID is required' });
+
+    if (!calendarIdVan) {
+        return res.status(500).json({ error: 'Calendar ID for Vancouver is missing' });
     }
 
-    try {
-        const events = await fetchGoogleCalendarEvents(calendarId);
+    if (!calendarIdCal) {
+      return res.status(500).json({ error: 'Calendar ID for Calgary is missing' });
+  }
 
-        res.status(200).json({ events });
+    try {
+        const [vanEvents, calEvents] = await Promise.all([
+            fetchGoogleCalendarEvents(calendarIdVan),
+            fetchGoogleCalendarEvents(calendarIdCal)
+        ]);
+
+        console.log('Vancouver events:', vanEvents);
+        console.log('Calgary events:', calEvents);
+
+        res.status(200).json({ message: 'Calendar events fetched successfully' });
     } catch (error) {
         console.error('Error fetching calendar events:', error.message);
         res.status(500).json({ error: 'Failed to fetch calendar events' });
     }
+
+
 } 
+
