d2bdb043cae5c1b95e5714cb837d1f1ee4928da3
R Lee <alpha.r.lee.dev@gmail.com>
2026-01-31 16:34:04 -0700

    Add eselint, formatting

--- initializeWatch.ts
+++ initializeWatch.ts
@@ -2,20 +2,20 @@ import { setupCalendarWatch } from './services/calendarService';
 import 'dotenv/config';
 
 async function initializeWatchChannels() {
-    const calendarIdVan = process.env.VANCOUVER_CALENDAR_ID;
-    const calendarIdCal = process.env.CALGARY_CALENDAR_ID;
+  const calendarIdVan = process.env.VANCOUVER_CALENDAR_ID;
+  const calendarIdCal = process.env.CALGARY_CALENDAR_ID;
 
-    if (!calendarIdVan || !calendarIdCal) {
-        throw new Error('Calendar IDs are missing');
-    }
+  if (!calendarIdVan || !calendarIdCal) {
+    throw new Error('Calendar IDs are missing');
+  }
 
-    try {
-        await setupCalendarWatch(calendarIdVan);
-        await setupCalendarWatch(calendarIdCal);
-        console.log('Watch channels initialized.');
-    } catch (error: any) {
-        console.error('Error initializing watch channels:', error.message);
-    }
+  try {
+    await setupCalendarWatch(calendarIdVan);
+    await setupCalendarWatch(calendarIdCal);
+    console.log('Watch channels initialized.');
+  } catch (error: any) {
+    console.error('Error initializing watch channels:', error.message);
+  }
 }
 
 initializeWatchChannels();
--- shared/helpers.ts
+++ shared/helpers.ts
@@ -2,24 +2,23 @@ import moment from 'moment-timezone';
 import crypto from 'crypto';
 
 export function extractEventLink(description: string): string | null {
-    if (!description) {
-      return null;
-    }
-  
-    const urlRegex = /(https?:\/\/[^\s">]+)/g;
-  
-    const matches = description.match(urlRegex);
-  
-    return matches ? matches[0] : null;
+  if (!description) {
+    return null;
   }
+
+  const urlRegex = /(https?:\/\/[^\s">]+)/g;
+
+  const matches = description.match(urlRegex);
+
+  return matches ? matches[0] : null;
+}
 
 export function getTimezone(timezone: string): string {
-    const now = new Date();
-    return moment.tz(now, timezone).format('z');
-
-  }
+  const now = new Date();
+  return moment.tz(now, timezone).format('z');
+}
 
 export function generateDocumentKey(eventId: string, calUID: string): string {
-  const combinedString = `${eventId}-${calUID}`
-  return crypto.createHash('sha256').update(combinedString).digest('hex')
-} 
+  const combinedString = `${eventId}-${calUID}`;
+  return crypto.createHash('sha256').update(combinedString).digest('hex');
+}
